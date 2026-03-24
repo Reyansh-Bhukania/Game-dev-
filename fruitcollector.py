b5e894839644ef3b89d06cbee7c6d0b4f022b731
Reyansh-Bhukania <bhukaniareyansh@gmail.com>
2026-03-24 21:06:51 +0530

    Add files via upload

--- fruitcollector.py
+++ fruitcollector.py
@@ -1,85 +1,84 @@
 import pgzrun 
 import random
-HEIGHT=500
-WIDTH=600
-basket=Actor("baskett",(300,400))
-fruitlist=[]
-fruits=["apple","dragonfruit","strawberry"]
-score=0
-def draw():
-    screen.blit("background",(0,0))
-    basket.draw()
-    for fruit in fruitlist:
-      fruit.draw()
-    screen.draw.text(str(score),center=(10,10))
-def new_fruit():
-    global fruit
-    fruit=Actor(random.choice(fruits),(random.randint(50,550),10))
-    fruitlist.append(fruit)
 
-    clock.schedule(new_fruit,1)
+HEIGHT = 500
+WIDTH = 600
+
+basket = Actor("basket", (300, 400))
+
+bomblist = []
+fruitlist = []
+
+fruits = ["apple", "dragonfruit", "strawberry", "banana"]
+gameover=False
+score = 0
+
+def draw():
+    if gameover==True:
+        screen.blit("gameover" , (0,0))
+        return
+    screen.blit("background", (0, 0))
+    basket.draw()
+    
+    for fruit in fruitlist:
+        fruit.draw()
+    
+    for bomb in bomblist:
+        bomb.draw()
+    
+    screen.draw.text("Score: " + str(score), (10, 10), color="white")
+
+def new_fruit():
+    fruit = Actor(random.choice(fruits), (random.randint(50, 550), 10))
+    fruitlist.append(fruit)
+    clock.schedule(new_fruit, 1)
+
+def new_bomb():
+    bomb = Actor("bomb", (random.randint(50, 550), 10))
+    bomblist.append(bomb)
+    clock.schedule(new_bomb, 3)
+
+
 
 def update():
+    global gameover
     global score
+
+  
     if keyboard.right:
-        basket.x+=5
-    
+        basket.x += 5
     if keyboard.left:
-        basket.x-=5 
-    for fruit in fruitlist:
-        fruit.y+=3
+        basket.x -= 5
 
-        if basket.colliderect (fruit):
-          fruitlist.remove(fruit)  
-          score+=1
-        if fruit.y>500:
-            fruitlist.remove(fruit) 
-            
+    for fruit in fruitlist[:]:
+        fruit.y += 3
 
+        if basket.colliderect(fruit):
+            fruitlist.remove(fruit)
+            score += 1
 
-
+        elif fruit.y > HEIGHT:
+            fruitlist.remove(fruit)
 
 
+    for bomb in bomblist[:]:
+        bomb.y += 4
+
+        if basket.colliderect(bomb):
+            bomblist.remove(bomb)
+            score -= 2   
+
+        elif bomb.y > HEIGHT:
+            bomblist.remove(bomb)
+
+    if score <0:
+        gameover=True
+
+        
 
 
 
 new_fruit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+new_bomb()
 
 pgzrun.go()
- 
